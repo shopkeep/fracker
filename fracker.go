314f6e8f3a89364dbb2b234bbf0cf798bca243fc
Sander Hartlage <sander@shopkeep.com>
2014-05-22 11:51:08 -0400

    add some documentation

--- fracker.go
+++ fracker.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	// TODO: remove dependence on etcd types, particularly *etcd.Response and *etcd.Node
 	"github.com/coreos/go-etcd/etcd"
 
 	"fmt"
@@ -9,15 +10,20 @@ import (
 	"strings"
 )
 
+// A Fracker drills into etcd directories and outputs the values to a given outlet.
 type Fracker interface {
+	// Frack() looks up each of the given keys in etcd and walks the tree of nodes returned. Each leaf
+	// value is converted into a environment variable declaration and written to the given io.Writer.
 	Frack(io.Writer, []string)
 }
 
+// New() creates a new Fracker.
 func New(client EtcdKeyGetter) Fracker {
 	return &fracker{client}
 }
 
-// this is just a wrapper to facilitate testing
+// The EtcdKeyGetter interface is to facilitate testing without using a running etcd process.
+// XXX: can this be unexported?
 type EtcdKeyGetter interface {
 	Get(string, bool, bool) (*etcd.Response, error)
 }
@@ -26,6 +32,8 @@ type fracker struct {
 	client EtcdKeyGetter
 }
 
+// Frack() reads the configuration values out of etcd and writes them to stdout. Panics if a requested key
+// is not found in etcd.
 func (self *fracker) Frack(out io.Writer, keys []string) {
 	var err error
 	var resp *etcd.Response
@@ -56,6 +64,7 @@ func (self *fracker) readNode(node *etcd.Node, fn func(string, string)) {
 }
 
 // turns an etcd key name "/foo/bar" into an environment variable name "FOO_BAR"
+// TODO: replace all non-valid characters with underscores
 func (self *fracker) envVarName(name string) string {
 	return strings.Replace(strings.ToUpper(name[1:]), "/", "_", -1)
 }
