2b1b6f7ef961d772191db1e95e3b682d20ce291a
Paul Guelpa <paul.guelpa@gmail.com>
2014-06-02 19:48:35 -0400

    Idea on how to refactor some of Fracker

--- fracker_test.go
+++ fracker_test.go
@@ -13,83 +13,20 @@ import (
 var _ = Describe("Fracker", func() {
 	var out *bytes.Buffer
 	var fracker f.Fracker
-	var client *StubEtcd
+	var client *TestClient
 	var err error
 
 	BeforeEach(func() {
 		out = bytes.NewBuffer([]byte{})
-		client = &StubEtcd{}
+		client = &TestClient{}
 		fracker = f.New(client)
 	})
 
-	Context(`fracking a key that doesn't exist`, func() {
-		BeforeEach(func() {
-			client.StubGet = func(key string) (f.Node, error) {
-				return nil, errors.New("no key")
-			}
-		})
-
-		JustBeforeEach(func() {
-			err = fracker.Frack(out, []string{"/foo"})
-		})
-
-		It(`returns an error`, func() {
-			Expect(err).ToNot(BeNil())
-		})
-	})
-
-	Context(`fracking a key that exists`, func() {
-		Context(`and is a file`, func() {
+	Describe(`Frack`, func() {
+		Context(`when a key doesn't exist`, func() {
 			BeforeEach(func() {
 				client.StubGet = func(key string) (f.Node, error) {
-					n := &etcd.Node{
-						Dir:   false,
-						Key:   "/foo/bar",
-						Value: "crunch",
-					}
-					return f.NewNode(n), nil
-				}
-			})
-
-			JustBeforeEach(func() {
-				err = fracker.Frack(out, []string{"/foo/bar"})
-			})
-
-			It(`does not return an error`, func() {
-				Expect(err).To(BeNil())
-			})
-
-			It(`writes the last segment of the node's value in KEY=VALUE format`, func() {
-				Expect(out.String()).To(Equal("BAR=crunch\n"))
-			})
-		})
-
-		Context(`and is a directory`, func() {
-			BeforeEach(func() {
-				client.StubGet = func(key string) (f.Node, error) {
-					n := &etcd.Node{
-						Dir: true,
-						Key: "/foo",
-						Nodes: []*etcd.Node{
-							&etcd.Node{
-								Dir: true,
-								Key: "/foo/bar",
-								Nodes: []*etcd.Node{
-									&etcd.Node{
-										Dir:   false,
-										Key:   "/foo/bar/baz",
-										Value: "crunch",
-									},
-									&etcd.Node{
-										Dir:   false,
-										Key:   "/foo/bar/qux",
-										Value: "munch",
-									},
-								},
-							},
-						},
-					}
-					return f.NewNode(n), nil
+					return nil, errors.New("no key")
 				}
 			})
 
@@ -97,12 +34,77 @@ var _ = Describe("Fracker", func() {
 				err = fracker.Frack(out, []string{"/foo"})
 			})
 
-			It(`doesn't return an error`, func() {
-				Expect(err).To(BeNil())
+			It(`returns an error`, func() {
+				Expect(err).ToNot(BeNil())
+			})
+		})
+
+		Context(`when a key does exist`, func() {
+			Context(`and is a file`, func() {
+				BeforeEach(func() {
+					client.StubGet = func(key string) (f.Node, error) {
+						n := &etcd.Node{
+							Dir:   false,
+							Key:   "/foo/baaaaaz",
+							Value: "crunch",
+						}
+						return f.NewNode(n), nil
+					}
+				})
+
+				JustBeforeEach(func() {
+					err = fracker.Frack(out, []string{"/foo/baaaaaz"})
+				})
+
+				It(`does not return an error`, func() {
+					Expect(err).To(BeNil())
+				})
+
+				It(`writes the last segment of the node's value in KEY=VALUE format`, func() {
+					Expect(out.String()).To(Equal("BAAAAAZ=crunch\n"))
+				})
 			})
 
-			It(`writes each value out in KEY=VALUE format (removing the prefix)`, func() {
-				Expect(out.String()).To(Equal("BAR_BAZ=crunch\nBAR_QUX=munch\n"))
+			Context(`and is a directory`, func() {
+				BeforeEach(func() {
+					client.StubGet = func(key string) (f.Node, error) {
+						n := &etcd.Node{
+							Dir: true,
+							Key: "/foo",
+							Nodes: []*etcd.Node{
+								&etcd.Node{
+									Dir: true,
+									Key: "/foo/bar",
+									Nodes: []*etcd.Node{
+										&etcd.Node{
+											Dir:   false,
+											Key:   "/foo/bar/baz",
+											Value: "crunch",
+										},
+										&etcd.Node{
+											Dir:   false,
+											Key:   "/foo/bar/qux",
+											Value: "munch",
+										},
+									},
+								},
+							},
+						}
+						return f.NewNode(n), nil
+					}
+				})
+
+				JustBeforeEach(func() {
+					err = fracker.Frack(out, []string{"/foo"})
+				})
+
+				It(`doesn't return an error`, func() {
+					Expect(err).To(BeNil())
+				})
+
+				It(`writes each value out in KEY=VALUE format (removing the prefix)`, func() {
+					Expect(out.String()).To(Equal("BAR_BAZ=crunch\nBAR_QUX=munch\n"))
+				})
 			})
 		})
 	})
--- node.go
+++ node.go
@@ -2,118 +2,27 @@ package fracker
 
 import (
 	"github.com/coreos/go-etcd/etcd"
-	"strings"
 )
 
-type KV interface {
-	Key() string
-	Value() string
-}
-
 type Node interface {
-	KV
 	Each(func(string, string))
-	Parent() Node
-	Node() *etcd.Node
 }
 
 func NewNode(n *etcd.Node) Node {
-	if n.Dir {
-		return &dirNodeClean{&node{n}}
-	} else {
-		return &fileNodeClean{&node{n}}
-	}
+	return &node{n}
 }
 
 type node struct {
 	n *etcd.Node
 }
 
-func (self *node) Node() *etcd.Node {
-	return self.n
-}
-
-func (self *node) Key() string {
-	return self.n.Key
-}
-
-func (self *node) Value() string {
-	return self.n.Value
-}
-
-type dirNodeClean struct {
-	*node
-}
-
-func (self *dirNodeClean) Node() *etcd.Node {
-	return self.node.Node()
-}
-
-func (self *dirNodeClean) Parent() Node {
-	return self
-}
-
-func (self *dirNodeClean) Each(fn func(string, string)) {
-	for _, n := range self.Node().Nodes {
-		if n.Dir {
-			dir := &dirNodeFromDir{&node{n}, self}
+func (self *node) Each(fn func(string, string)) {
+	if self.n.Dir {
+		for _, e_node := range self.n.Nodes {
+			dir := &node{e_node}
 			dir.Each(fn)
-		} else {
-			file := &fileNodeFromDir{&node{n}, self}
-			file.Each(fn)
 		}
+	} else {
+		fn(self.n.Key, self.n.Value)
 	}
-}
-
-type dirNodeFromDir struct {
-	*node
-	parent *dirNodeClean
-}
-
-func (self *dirNodeFromDir) Parent() Node {
-	return self.parent
-}
-
-func (self *dirNodeFromDir) Each(fn func(string, string)) {
-	for _, n := range self.Node().Nodes {
-		if n.Dir {
-			dir := &dirNodeFromDir{&node{n}, self.parent}
-			dir.Each(fn)
-		} else {
-			file := &fileNodeFromDir{&node{n}, self.parent}
-			file.Each(fn)
-		}
-	}
-}
-
-type fileNodeClean struct {
-	*node
-}
-
-func (self *fileNodeClean) Node() *etcd.Node {
-	return self.node.Node()
-}
-
-func (self *fileNodeClean) Each(fn func(string, string)) {
-	idx := strings.LastIndex(self.Key(), "/")
-	k := self.Key()[idx+1:]
-	fn(k, self.Value())
-}
-
-func (self *fileNodeClean) Parent() Node {
-	return nil
-}
-
-type fileNodeFromDir struct {
-	*node
-	parent Node
-}
-
-func (self *fileNodeFromDir) Each(fn func(string, string)) {
-	parent := self.Parent()
-	fn(strings.TrimPrefix(self.Key(), parent.Key())[1:], self.Value())
-}
-
-func (self *fileNodeFromDir) Parent() Node {
-	return self.parent
 }
